fcd5b862466176ea52fa065b97c658e0eb20ed7d
p6nj <breee@duck.com>
2024-03-19 23:41:32 +0100

    no need for tokio, structs & impls for UnixSocket

--- examples/nice.rs
+++ examples/nice.rs
@@ -6,10 +6,10 @@ fn main() -> std::io::Result<()> {
 
         // Receives a single datagram message on the socket. If `buf` is too small to hold
         // the message, it will be cut off.
-        let mut buf = [0; 10];
+        let mut buf = [0; 65527]; // max UDP datagram size according to https://stackoverflow.com/a/77043561
         let (amt, src) = socket.recv_from(&mut buf)?;
 
-        println!("From {} : {:?}", src, buf)
+        println!("From {} : {:?}", src, &buf[..amt])
     } // the socket is closed here
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use rodio::{cpal::FromSample, OutputStream, PlayError, Source, StreamError};
+use std::{
+    io,
+    marker::PhantomData,
+    os::unix::net::{SocketAddr, UnixListener, UnixStream},
+};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum BupError {
+    #[error("can't bind socket address")]
+    UnixSocket(#[from] io::Error),
+    #[error("can't open default output sound device")]
+    OutputStream(#[from] StreamError),
+    #[error("can't play a sound")]
+    Play(#[from] PlayError),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait Samples: Source + Send
+where
+    <Self as Iterator>::Item: rodio::Sample,
+{
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub trait Buzzer {
+    fn buzz<S: Samples>(&mut self, incoming: UnixStream) -> S
+    where
+        <S as Iterator>::Item: rodio::Sample;
+}
+pub struct Bup<S>(SocketAddr, PhantomData<S>)
+where
+    S: Samples,
+    <S as Iterator>::Item: rodio::Sample;
+
+impl<S> Bup<S>
+where
+    f32: FromSample<<S as Iterator>::Item>,
+    S: Samples + 'static,
+    <S as Iterator>::Item: rodio::Sample,
+{
+    pub fn activate<B: Buzzer>(&self, mut buzzer: B) -> Result<(), BupError>
+    where
+        <S as Iterator>::Item: rodio::Sample,
+    {
+        let listener = UnixListener::bind_addr(&self.0)?;
+        let (_stream, stream_handle) = OutputStream::try_default()?;
+        Ok(stream_handle.play_raw(
+            buzzer
+                .buzz::<S>(listener.accept().unwrap().0)
+                .convert_samples(),
+        )?)
     }
 }
